8899b6500beca70a2e0cf15bc9eaa418033e92e0
ray1888 <alal1995@hotmail.com>
2022-11-09 07:05:42 +0000

    feat(two_sum): add two sum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 pub mod is_palindrome_link_list;
+pub mod two_sum;
 pub mod zigzag;
 pub mod zigzag;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/two_sum/two_sum.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+        for (index, num) in nums.iter().enumerate() {
+            match map.get(&(target - num)) {
+                None => {
+                    map.insert(num, index);
+                }
+                Some(index2) => return vec![*index2 as i32, index as i32],
+            }
+        }
+        vec![]
+    }
+}
