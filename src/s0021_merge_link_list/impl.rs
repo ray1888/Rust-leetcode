f2494ec0d43491d8aa4f90392e9cfcf25a6e1ee8
ray1888 <alal1995@hotmail.com>
2023-03-02 13:40:19 +0000

    feat(solution): 160

--- src/s0021_merge_link_list/impl.rs
+++ src/s0021_merge_link_list/impl.rs
@@ -19,14 +19,17 @@ impl Solution {
         mut l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
         let mut dummy = ListNode::new(0);
+        // 因为这里dummy只是给了p一个可变借用，因此最后的时候，dummy.next是可以调用的，所有权没有迁移
         let mut p = &mut dummy;
         while l1.is_some() && l2.is_some() {
             let (p1, p2) = (l1.as_deref_mut().unwrap(), l2.as_deref_mut().unwrap());
             if p1.val < p2.val {
+                // Option.Take需要用&mut self ，所以l1拿出来的必须是mut，所以导致l1也要变成mut
                 let next = p1.next.take();
                 p.next = l1.take();
                 l1 = next;
             } else {
+                // Option.Take需要用&mut self 
                 let next = p2.next.take();
                 p.next = l2.take();
                 l2 = next;
