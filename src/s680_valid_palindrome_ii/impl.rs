8eb08c8e048a5f127e1b7f59e01e0629f2b1cd84
ray1888 <alal1995@hotmail.com>
2023-03-11 05:59:07 +0000

    feat(s0680): add solution

--- src/s680_valid_palindrome_ii/impl.rs
+++ src/s680_valid_palindrome_ii/impl.rs
@@ -1,34 +1,67 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn is_palindrome(s: String) -> bool {
-       
-        let list: Vec<char> = s.to_lowercase().chars().collect();
-        let mut left = 0;
-        let mut right = list.len() -1 ;
-        
+    pub fn valid_palindrome(s: String) -> bool {
+        let mut low = 0;
+        let mut high = s.len()-1;
 
-        while left < right{
-            let mut v1 = list.get(left).unwrap();
-            let mut v2 = list.get(right).unwrap();
-
-            while left < right && !v1.is_alphanumeric()  {
-                left+=1;
-                v1 = list.get(left).unwrap();
-            }
-            while left < right && !v2.is_alphanumeric() {
-                right-=1;
-                v2 = list.get(right).unwrap();
-            }
-
-            if left < right{
-                if v1 != v2 {
-                    return false;
+        while low < high {
+            if s.chars().nth(i).unwrap() == s.chars().nth(j).unwrap(){
+                low+=1;
+                high-=1;
+            } else {
+                let mut flag1 = true;
+                let mut flag2 = true;
+                let mut i = low;
+                let mut j = high-1;
+                while i < j {
+                    if s.chars().nth(i).unwrap() != s.chars().nth(j).unwrap(){
+                        flag1 = false;
+                        break;
+                    }
+                    i+=1;
+                    j-=1;
                 }
-                left+=1;
-                right-=1;
+                i = low +1;
+                j = high;
+                while i < j {
+                    if s.chars().nth(i).unwrap() != s.chars().nth(j).unwrap(){
+                        flag2 = false;
+                        break;
+                    }
+                    i+=1;
+                    j-=1;
+                }
+                return flag1 || flag2
             }
         }
         return true;
+    }
+}
+
+impl Solution {
+    pub fn valid_palindrome(s: String) -> bool {
+        #[inline(always)]
+        fn is(s: &Vec<u8>, mut i: usize, mut j: usize) -> bool {
+            while j > i {
+                if unsafe { s.get_unchecked(i) == s.get_unchecked(j) } {
+                    i += 1;
+                    j -= 1;
+                } else {
+                    return false;
+                }
+            }
+            true
+        }
+        let (mut i, mut j, bs) = (0usize, s.len() - 1, s.into_bytes());
+        while j > i {
+            if unsafe { bs.get_unchecked(i) == bs.get_unchecked(j) } {
+                i += 1;
+                j -= 1;
+            } else {
+                return is(&bs, i, j - 1) || is(&bs, i + 1, j);
+            }
+        }
+        true
     }
 }
